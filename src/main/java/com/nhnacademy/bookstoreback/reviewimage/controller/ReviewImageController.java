4263302c3c4afc14eab749c3a6fb58a0e0b7c743
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-18 00:06:43 +0900

    feat: CRUD 추가

--- src/main/java/com/nhnacademy/bookstoreback/reviewimage/controller/ReviewImageController.java
+++ src/main/java/com/nhnacademy/bookstoreback/reviewimage/controller/ReviewImageController.java
@@ -1,14 +1,58 @@
 package com.nhnacademy.bookstoreback.reviewimage.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nhnacademy.bookstoreback.reviewimage.domain.entity.ReviewImage;
 import com.nhnacademy.bookstoreback.reviewimage.service.ReviewImageService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/reviews/{reviewId}/reviewImage")
 public class ReviewImageController {
 	private final ReviewImageService reviewImageService;
+
+	@GetMapping
+	public ResponseEntity<List<ReviewImage>> getReviewImages(@PathVariable Long reviewId) {
+		List<ReviewImage> reviewImages = reviewImageService.getReviewImages();
+		return ResponseEntity.status(HttpStatus.OK).body(reviewImages);
+	}
+
+	@PostMapping
+	public ResponseEntity<ReviewImage> createReviewImage(@PathVariable Long reviewId,
+		@RequestBody ReviewImage reviewImage) {
+		ReviewImage reviewImage1 = reviewImageService.saveReviewImage(reviewImage);
+		return ResponseEntity.status(HttpStatus.CREATED).body(reviewImage1);
+	}
+
+	@GetMapping("/{reviewImageId}")
+	public ResponseEntity<ReviewImage> getReviewImage(@PathVariable Long reviewImageId) {
+		ReviewImage reviewImage = reviewImageService.getReviewImageById(reviewImageId);
+		return ResponseEntity.status(HttpStatus.OK).body(reviewImage);
+	}
+
+	@PutMapping
+	public ResponseEntity<ReviewImage> updateReviewImage(@PathVariable Long reviewId,
+		@RequestBody ReviewImage reviewImage) {
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping
+	public ResponseEntity<ReviewImage> deleteReviewImage(@PathVariable Long reviewId) {
+		reviewImageService.deleteReviewImage(reviewId);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- src/main/java/com/nhnacademy/bookstoreback/reviewimage/service/impl/ReviewImageServiceImpl.java
+++ src/main/java/com/nhnacademy/bookstoreback/reviewimage/service/impl/ReviewImageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.bookstoreback.reviewimage.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.nhnacademy.bookstoreback.reviewimage.domain.entity.ReviewImage;
 import com.nhnacademy.bookstoreback.reviewimage.repository.ReviewImageRepository;
 import com.nhnacademy.bookstoreback.reviewimage.service.ReviewImageService;
 
@@ -11,4 +14,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReviewImageServiceImpl implements ReviewImageService {
 	private final ReviewImageRepository reviewImageRepository;
+
+	@Override
+	public List<ReviewImage> getReviewImages() {
+		return reviewImageRepository.findAll();
+	}
+
+	@Override
+	public ReviewImage saveReviewImage(ReviewImage reviewImage) {
+		return reviewImageRepository.save(reviewImage);
+	}
+
+	@Override
+	public ReviewImage getReviewImageById(Long reviewImageId) {
+		return reviewImageRepository.findById(reviewImageId).orElse(null);
+	}
+
+	@Override
+	public ReviewImage updateReviewImage(ReviewImage reviewImage) {
+		return null;
+	}
+ 
+	@Override
+	public void deleteReviewImage(Long reviewImageId) {
+		reviewImageRepository.deleteById(reviewImageId);
+	}
 }
