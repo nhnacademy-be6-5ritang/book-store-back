14fcfc74c843cbbec8792ad1cac4a2c753638815
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-18 00:37:38 +0900

    CR 추가

--- src/main/java/com/nhnacademy/bookstoreback/cart/controller/CartController.java
+++ src/main/java/com/nhnacademy/bookstoreback/cart/controller/CartController.java
@@ -1,4 +1,33 @@
 package com.nhnacademy.bookstoreback.cart.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nhnacademy.bookstoreback.cart.domain.entity.Cart;
+import com.nhnacademy.bookstoreback.cart.service.CartService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/carts")
 public class CartController {
+	private final CartService cartService;
+
+	@PutMapping
+	public ResponseEntity<Cart> createCart(@RequestBody Cart cart) {
+		Cart cart1 = cartService.createCart(cart);
+		return ResponseEntity.status(HttpStatus.CREATED).body(cart1);
+	}
+
+	@GetMapping
+	public ResponseEntity<Cart> getCart(Long cartId) {
+		Cart cart = cartService.getCart(cartId);
+		return ResponseEntity.ok(cart);
+	}
 }
--- src/main/java/com/nhnacademy/bookstoreback/cart/service/CartService.java
+++ src/main/java/com/nhnacademy/bookstoreback/cart/service/CartService.java
@@ -1,4 +1,9 @@
 package com.nhnacademy.bookstoreback.cart.service;
 
+import com.nhnacademy.bookstoreback.cart.domain.entity.Cart;
+
 public interface CartService {
+	Cart createCart(Cart cart);
+
+	Cart getCart(Long cardId);
 }
