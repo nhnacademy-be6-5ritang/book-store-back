151d1fd423546f372ba3a709f0e68f7d7151ae06
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-18 14:39:31 +0900

    feat: 테이블 이름 설정

--- src/main/java/com/nhnacademy/bookstoreback/review/domain/entity/Review.java
+++ src/main/java/com/nhnacademy/bookstoreback/review/domain/entity/Review.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "reviews")
 public class Review {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
