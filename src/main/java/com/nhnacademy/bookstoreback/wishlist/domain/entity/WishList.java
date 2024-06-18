90f50a844bef91c1d8f1a7a3fa4e5b18ebf461e4
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-18 14:42:06 +0900

    feat: 테이블 이름 설정

--- src/main/java/com/nhnacademy/bookstoreback/wishlist/domain/entity/WishList.java
+++ src/main/java/com/nhnacademy/bookstoreback/wishlist/domain/entity/WishList.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "wish_lists")
 public class WishList {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
