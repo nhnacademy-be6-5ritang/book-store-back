36cb2066b1c3f48552b60529d06d2ce825636c2d
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-19 01:04:29 +0900

    feat: Cart.java 회원 매핑 설정

--- src/main/java/com/nhnacademy/bookstoreback/cart/domain/entity/Cart.java
+++ src/main/java/com/nhnacademy/bookstoreback/cart/domain/entity/Cart.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.bookstoreback.cart.domain.entity;
 
+import com.nhnacademy.bookstoreback.user.domain.entity.User;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,8 +24,8 @@ public class Cart {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long cartId;
 
-	// @ManyToOne(optional = false)
-	// @JoinColumn(name = "user_id")
-	// private User user;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "user_id")
+	private User user;
 
 }
