5e43593cd7dec479e46906b84f30d32d82b0c2e2
kimdaun <fhdlwp15@naver.com>
2024-06-20 09:37:45 +0900

    refactor: 보내주는 정보를 order 객체에서 GetOrderResponse로 변경

--- src/main/java/com/nhnacademy/bookstoreback/order/domain/dto/response/GetOrderByStatusIdResponse.java
+++ src/main/java/com/nhnacademy/bookstoreback/order/domain/dto/response/GetOrderByStatusIdResponse.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.bookstoreback.order.domain.dto.response;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -10,12 +11,16 @@ import lombok.Builder;
 
 @Builder
 public record GetOrderByStatusIdResponse(
-	List<Order> orders,
+	List<GetOrderResponse> orders,
 	int pageNumber
 ) {
 	public static GetOrderByStatusIdResponse from(Page<Order> order) {
+		GetOrderResponse orderResponse = null;
+		for (Order orderItem : order) {
+			orderResponse = GetOrderResponse.from(orderItem);
+		}
 		return GetOrderByStatusIdResponse.builder()
-			.orders(order.getContent())
+			.orders(Collections.singletonList(orderResponse))
 			.pageNumber(order.getNumber())
 			.build();
 	}
