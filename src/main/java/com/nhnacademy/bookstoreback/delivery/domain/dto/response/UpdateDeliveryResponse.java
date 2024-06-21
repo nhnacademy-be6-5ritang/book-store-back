50d3f97623e6123ccfc0ad70903aee5700d2bce7
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-21 12:46:23 +0900

    feat: delivery, http 요청 테스트 추가

--- src/main/java/com/nhnacademy/bookstoreback/delivery/domain/dto/response/UpdateDeliveryResponse.java
+++ src/main/java/com/nhnacademy/bookstoreback/delivery/domain/dto/response/UpdateDeliveryResponse.java
@@ -1,9 +1,7 @@
 package com.nhnacademy.bookstoreback.delivery.domain.dto.response;
 
-import com.nhnacademy.bookstoreback.order.domain.entity.Order;
-
 import lombok.Builder;
 
 @Builder
-public record UpdateDeliveryResponse(Order order, String deliveryStatusName) {
+public record UpdateDeliveryResponse(Long deliveryId, Long orderId, String deliveryStatusName) {
 }
