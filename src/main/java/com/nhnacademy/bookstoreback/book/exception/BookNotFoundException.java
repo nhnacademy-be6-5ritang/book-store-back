18970af36a27657db382814d28b53de704aef255
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-02 16:49:31 +0900

    feat: 예외처리 수정중

---
+++ src/main/java/com/nhnacademy/bookstoreback/book/exception/BookNotFoundException.java
@@ -0,0 +1,11 @@
+// package com.nhnacademy.bookstoreback.book.exception;
+//
+// import com.nhnacademy.bookstoreback.global.exception.NotFoundException;
+// import com.nhnacademy.bookstoreback.global.exception.payload.ErrorStatus;
+//
+// public class BookNotFoundException extends NotFoundException(){
+//
+// public BookNotFoundException(ErrorStatus errorStatus) {
+// 	super(errorStatus);
+// }
+// }
