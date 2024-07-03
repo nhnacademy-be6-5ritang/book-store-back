036fa7214aeb8caa0739ef39bf2ae32eb6d8bf20
kimdaun <fhdlwp15@naver.com>
2024-07-03 11:05:45 +0900

    refactor : 익셉션 처리 및 파일 구조 변경

--- src/main/java/com/nhnacademy/bookstoreback/order/service/WrappingPaperService.java
+++ src/main/java/com/nhnacademy/bookstoreback/order/service/WrappingPaperService.java
@@ -1,47 +1,12 @@
 package com.nhnacademy.bookstoreback.order.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.nhnacademy.bookstoreback.order.domain.dto.response.GetListWrappingResponse;
 import com.nhnacademy.bookstoreback.order.domain.dto.response.GetWrappingResponse;
-import com.nhnacademy.bookstoreback.order.domain.entity.BookOrder;
-import com.nhnacademy.bookstoreback.order.domain.entity.PaperType;
-import com.nhnacademy.bookstoreback.order.domain.entity.WrappingPaper;
-import com.nhnacademy.bookstoreback.order.repository.BookOrderRepository;
-import com.nhnacademy.bookstoreback.order.repository.PaperTypeRepository;
-import com.nhnacademy.bookstoreback.order.repository.WrappingPaperRepository;
 
-import lombok.RequiredArgsConstructor;
+public interface WrappingPaperService {
+	GetWrappingResponse createWrappingPapers(Long paperId, Long bookOrderId, Integer quantity);
 
-@RequiredArgsConstructor
-@Service
-@Transactional
-public class WrappingPaperService {
+	void deleteWrappingPapers(Long id);
 
-	private final WrappingPaperRepository wrappingPaperRepository;
-	private final PaperTypeRepository paperTypeRepository;
-	private final BookOrderRepository bookOrderRepository;
-
-	public GetWrappingResponse createWrappingPapers(Long paperId, Long bookOrderId, Integer quantity) {
-		BookOrder bookOrder = null;
-		if (bookOrderId != null) {
-			bookOrder = bookOrderRepository.getReferenceById(bookOrderId);
-		}
-		PaperType paperType = paperTypeRepository.findById(paperId).orElse(null);
-		if (quantity != null) {
-			return GetWrappingResponse.from(
-				wrappingPaperRepository.save(WrappingPaper.toEntity(bookOrder, paperType, quantity)));
-		}
-		return GetWrappingResponse.from(
-			wrappingPaperRepository.save(WrappingPaper.toEntity(bookOrder, paperType, null)));
-	}
-
-	public void deleteWrappingPapers(Long id) {
-		wrappingPaperRepository.deleteById(id);
-	}
-
-	public GetListWrappingResponse getWrappingPaperByOrderListId(Long id) {
-		return GetListWrappingResponse.from(wrappingPaperRepository.findAllByBookOrder_OrderListId(id));
-	}
+	GetListWrappingResponse getWrappingPaperByOrderListId(Long id);
 }
--- src/main/java/com/nhnacademy/bookstoreback/payment/controller/PaymentController.java
+++ src/main/java/com/nhnacademy/bookstoreback/payment/controller/PaymentController.java
@@ -15,7 +15,7 @@ import com.nhnacademy.bookstoreback.payment.dto.response.CancelResponse;
 import com.nhnacademy.bookstoreback.payment.dto.response.PaymentSaveResponse;
 import com.nhnacademy.bookstoreback.payment.dto.response.TransactionsResponse;
 import com.nhnacademy.bookstoreback.payment.dto.response.UpdatePaymentResponse;
-import com.nhnacademy.bookstoreback.payment.service.PaymentService;
+import com.nhnacademy.bookstoreback.payment.service.Impl.PaymentServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,38 +26,39 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class PaymentController {
 
-	private final PaymentService paymentService;
+	private final PaymentServiceImpl paymentServiceImpl;
 
 	//주문 보안 아이디로 주문리스트 가져오기
 	@GetMapping("/books-orders/{order_info_id}")
 	public ResponseEntity<GetBookOrderByInfoIdResponse> bookOrder(@PathVariable("order_info_id") String orderInfoId) {
-		return ResponseEntity.status(HttpStatus.OK).body(paymentService.findByOrderInfoId(orderInfoId));
+		return ResponseEntity.status(HttpStatus.OK).body(paymentServiceImpl.findByOrderInfoId(orderInfoId));
 	}
 
 	@GetMapping("/order-info/{order_info_id}")
 	public ResponseEntity<GetOrderByInfoResponse> findByOrderInfoId(@PathVariable("order_info_id") String orderInfoId) {
-		return ResponseEntity.status(HttpStatus.OK).body(paymentService.findByOrder(orderInfoId));
+		return ResponseEntity.status(HttpStatus.OK).body(paymentServiceImpl.findByOrder(orderInfoId));
 	}
 
 	@PostMapping
 	public ResponseEntity<PaymentSaveResponse> savePayment(@RequestBody String paymentResponseJson) {
-		return ResponseEntity.status(HttpStatus.OK).body(paymentService.savePaymentResponse(paymentResponseJson));
+		return ResponseEntity.status(HttpStatus.OK).body(paymentServiceImpl.savePaymentResponse(paymentResponseJson));
 	}
 
 	@PostMapping("/transactions")
 	public ResponseEntity<TransactionsResponse> transactions(@RequestBody String paymentResponseJson) {
-		return ResponseEntity.status(HttpStatus.OK).body(paymentService.transactions(paymentResponseJson));
+		return ResponseEntity.status(HttpStatus.OK).body(paymentServiceImpl.transactions(paymentResponseJson));
 	}
 
 	@GetMapping("/cancel/{order_info_id}")
 	public ResponseEntity<CancelResponse> cancel(@PathVariable("order_info_id") String orderInfoId) {
-		return ResponseEntity.status(HttpStatus.OK).body(paymentService.paymentFindByOrderInfoId(orderInfoId));
+		return ResponseEntity.status(HttpStatus.OK).body(paymentServiceImpl.paymentFindByOrderInfoId(orderInfoId));
 	}
 
 	@PostMapping("/cancel/test/{payment_id}")
 	public ResponseEntity<UpdatePaymentResponse> cancel(@RequestBody String paymentResponseJson,
 		@PathVariable("payment_id") Long paymentId) {
-		return ResponseEntity.status(HttpStatus.OK).body(paymentService.updatePayment(paymentResponseJson, paymentId));
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(paymentServiceImpl.updatePayment(paymentResponseJson, paymentId));
 	}
 
 }
