7b8748bf6260a02af58c2601c9eabfad54a9b277
kimdaun <fhdlwp15@naver.com>
2024-07-09 08:57:20 +0900

    refactor : 카트 레포 수정

--- src/main/java/com/nhnacademy/bookstoreback/cart/repository/CartRepository.java
+++ src/main/java/com/nhnacademy/bookstoreback/cart/repository/CartRepository.java
@@ -1,11 +1,15 @@
 package com.nhnacademy.bookstoreback.cart.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nhnacademy.bookstoreback.cart.domain.entity.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 	boolean existsByUserId(Long userId);
+
+	Optional<Cart> findByUserId(Long userId);
 
 	Cart findByUser_Id(Long userId);
 }
