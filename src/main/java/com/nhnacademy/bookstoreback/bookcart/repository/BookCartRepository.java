3db06cb83c8b5dd913cc210dbc932f8ecc347021
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-14 11:51:30 +0900

    fix: redis test4

--- src/main/java/com/nhnacademy/bookstoreback/bookcart/repository/BookCartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.bookstoreback.bookcart.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nhnacademy.bookstoreback.bookcart.domain.entity.BookCart;
-
-public interface BookCartRepository extends CrudRepository<BookCart, String> {
-}
