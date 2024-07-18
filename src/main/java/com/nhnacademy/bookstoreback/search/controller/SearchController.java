8d2dd396242e9e2318006e63816679795b69c718
giuk <2897robo@naver.com>
2024-07-18 15:45:56 +0900

    feature : 검색기능 테스트

---
+++ src/main/java/com/nhnacademy/bookstoreback/search/controller/SearchController.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.bookstoreback.search.controller;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+import com.nhnacademy.bookstoreback.search.service.SearchService;
+
+@RestController
+public class SearchController {
+
+	@Autowired
+	private SearchService searchService;
+
+	@GetMapping("/search/books")
+	public SearchResponse searchBooks(@RequestParam String query) throws IOException {
+		return searchService.searchBooks(query);
+	}
+
+	@GetMapping("/search/authors")
+	public SearchResponse searchAuthors(@RequestParam String query) throws IOException {
+		return searchService.searchAuthors(query);
+	}
+
+	// 추가로 다른 엔티티에 대한 검색 엔드포인트를 구현할 수 있습니다.
+}
