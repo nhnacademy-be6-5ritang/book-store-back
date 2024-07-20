6f1279f962ae9cb1b12ec65750efe748ae42d024
giuk <2897robo@naver.com>
2024-07-21 01:03:58 +0900

    feature : 검색기능 테스트

--- src/main/java/com/nhnacademy/bookstoreback/search/service/SearchService.java
+++ src/main/java/com/nhnacademy/bookstoreback/search/service/SearchService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.logging.Logger;
 
 @Service
@@ -23,9 +24,9 @@ public class SearchService {
 	public SearchResponse searchBooks(String query) throws IOException {
 		logger.info("Searching for books with query: " + query);
 
-		SearchRequest searchRequest = new SearchRequest("books", "index-author", "index-category", "index-publisher", "index-tag");
+		SearchRequest searchRequest = new SearchRequest("books");
 		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-		sourceBuilder.query(QueryBuilders.multiMatchQuery(query)
+		sourceBuilder.query(QueryBuilders.multiMatchQuery(query, "book_title", "book_description", "book_isbn")
 			.type("best_fields"));
 		searchRequest.source(sourceBuilder);
 
@@ -34,4 +35,40 @@ public class SearchService {
 
 		return response;
 	}
+
+	public SearchResponse searchAuthors(String query) throws IOException {
+		logger.info("Searching for authors with query: " + query);
+
+		SearchRequest searchRequest = new SearchRequest("index-author");
+		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+		sourceBuilder.query(QueryBuilders.matchQuery("author_name", query));
+		searchRequest.source(sourceBuilder);
+
+		SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+		String authorId = null;
+
+		// Author ID 추출
+		if (response.getHits().getTotalHits().value > 0) {
+			authorId = response.getHits().getHits()[0].getId(); // 첫 번째 저자 ID 사용
+		}
+
+		if (authorId == null) {
+			return response; // 저자가 없을 경우 빈 리스트 반환
+		}
+
+		// Step 2: Book 검색
+		SearchRequest bookSearchRequest = new SearchRequest("books");
+		SearchSourceBuilder bookSourceBuilder = new SearchSourceBuilder();
+		bookSourceBuilder.query(QueryBuilders.termQuery("author_id", authorId));
+		bookSearchRequest.source(bookSourceBuilder);
+
+		SearchResponse bookResponse = client.search(bookSearchRequest, RequestOptions.DEFAULT);
+
+		logger.info("Search response: " + response.toString());
+
+		return bookResponse;
+	}
+
+
+	// 추가로 다른 엔티티에 대한 검색 메서드를 구현할 수 있습니다.
 }
