b6757ef2a8c0cb125af2b54b716f8c2574589b70
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-25 14:21:28 +0900

    feat: Swagger 설정 및 Author 내용 추가

---
+++ src/main/java/com/nhnacademy/bookstoreback/global/controller/SwaggerController.java
@@ -0,0 +1,39 @@
+package com.nhnacademy.bookstoreback.global.controller;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import org.springdoc.webmvc.api.OpenApiResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/coupons/api")
+public class SwaggerController {
+	private final OpenApiResource openApiResource;
+
+	@GetMapping()
+	public ResponseEntity<String> getSwaggerJson(HttpServletRequest request) throws JsonProcessingException {
+		String apiDocsUrl = "/back-docs/back-api"; // Swagger 문서 URL
+		Locale locale = request.getLocale(); // 현재 요청의 Locale
+
+		byte[] swaggerJsonBytes = openApiResource.openapiJson(request, apiDocsUrl, locale);
+		if (swaggerJsonBytes == null) {
+			return ResponseEntity.ok("{}"); // 빈 JSON 반환
+		}
+
+		String swaggerJson = new String(swaggerJsonBytes, StandardCharsets.UTF_8);
+		return ResponseEntity.ok(swaggerJson);
+	}
+}
+
+
+
