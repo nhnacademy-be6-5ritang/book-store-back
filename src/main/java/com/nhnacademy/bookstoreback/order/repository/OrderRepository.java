c4cd112c29dbec179c103c776344af3d6a48c0a0
Kim Gi Uk <2897robo@naver.com>
2024-07-25 16:25:45 +0900

    Revert "feature : 유저 주문 정보 페이징 처리"

--- src/main/java/com/nhnacademy/bookstoreback/order/repository/OrderRepository.java
+++ src/main/java/com/nhnacademy/bookstoreback/order/repository/OrderRepository.java
@@ -18,6 +18,4 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 	Order findByOrderId(Long orderId);
 
 	List<Order> findAllByOrderStatus_OrderStatusId(Long orderStatusId);
-
-	Page<Order> findAllByUser_Id(Long userId, Pageable page);
 }
