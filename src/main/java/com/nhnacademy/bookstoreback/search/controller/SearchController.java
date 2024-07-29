a42418f8649ac279d380091b27a937f96f9d1557
giuk <2897robo@naver.com>
2024-07-29 13:56:05 +0900

    feature : 검색 정렬 기능 구현

--- src/main/java/com/nhnacademy/bookstoreback/search/controller/SearchController.java
+++ src/main/java/com/nhnacademy/bookstoreback/search/controller/SearchController.java
@@ -38,18 +38,18 @@ public class SearchController {
 	@GetMapping("/authors")
 	public ResponseEntity<Page<BookSearchResponse>> searchAuthors(@RequestParam String query, @PageableDefault(page = 1, size = 20) Pageable pageable) throws IOException {
 		logger.info("Service Received request to search authors with query: " + query);
-		return ResponseEntity.status(HttpStatus.OK).body(searchService.searchBooks(query, pageable));
+		return ResponseEntity.status(HttpStatus.OK).body(searchService.searchAuthors(query, pageable));
 	}
 
 	@GetMapping("/publisher")
 	public ResponseEntity<Page<BookSearchResponse>> searchPublishers(@RequestParam String query, @PageableDefault(page = 1, size = 20) Pageable pageable) throws IOException {
 		logger.info("Service Received request to search publishers with query: " + query);
-		return ResponseEntity.status(HttpStatus.OK).body(searchService.searchBooks(query, pageable));
+		return ResponseEntity.status(HttpStatus.OK).body(searchService.searchPublishers(query, pageable));
 	}
 
 	@GetMapping("/tag")
 	public ResponseEntity<Page<BookSearchResponse>> searchBooksByTag(@RequestParam String query, @PageableDefault(page = 1, size = 20) Pageable pageable) throws IOException {
 		logger.info("Service Received request to search books by tag with query: " + query);
-		return ResponseEntity.status(HttpStatus.OK).body(searchService.searchBooks(query, pageable));
+		return ResponseEntity.status(HttpStatus.OK).body(searchService.searchBooksByTag(query, pageable));
 	}
 }
