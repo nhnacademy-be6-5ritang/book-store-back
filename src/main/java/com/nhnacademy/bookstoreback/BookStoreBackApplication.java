0ca801e125e9e86b14c5881286b3e749b591a33f
taehwan01 <monte6198@gmail.com>
2024-11-19 06:10:00 +0900

    fix: Page JSON 직렬화 이슈 해결

--- src/main/java/com/nhnacademy/bookstoreback/BookStoreBackApplication.java
+++ src/main/java/com/nhnacademy/bookstoreback/BookStoreBackApplication.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.bookstoreback;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -7,6 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -18,6 +21,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @ConfigurationPropertiesScan
 @EnableDiscoveryClient
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class BookStoreBackApplication {
 
 	public static void main(String[] args) {
